06ad2ca0aa1d482dfd4799d8ec6c4219efb52433
Matt | Varghese Mathew <varghese85@gmail.com>
2015-12-18 21:21:58 -0600

    added documentation

--- 01toggleLED.c
+++ 01toggleLED.c
@@ -1,7 +1,28 @@
+/* Author: Matt | Varghese Mathew
+ * Copyright 2015 Varghese Mathew
+ * Youtube video: https://www.youtube.com/watch?v=KXMdAYQf3QA
+ * License: GNU General Public License
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Compile Command:
+ *  gcc 01toggleLED.c -o 01toggleLED -l wiringPi
+ */
 #include <wiringPi.h>
 #include <stdio.h>
 
+// RPi2 GPIO17 maps to pin 0
 #define LedPin    0
+// RPi2 GPIO18 maps to pin 1
 #define ButtonPin 1
 
 int state = 0;         // used to track if LED is on or off.
